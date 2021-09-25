57c60c4f0a65ecfd82db7af2ad20ce72d97f3c27
Cristian Muñoz <cris091187965@gmail.com>
2021-09-25 17:18:37 -0300

    Curso 3 terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -225,10 +225,7 @@ function esVocal(letra){
   //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
-  //Escribe tu código aquí
-  if (letra.length > 1) {
-    return "Dato incorrecto";
-  }
+  //Escribe tu código aquícd
   let vocales = ["a","e","i","o","u"];
   for (let i = 0; i < vocales.length; i++) {
     if (letra === vocales[i]) {
